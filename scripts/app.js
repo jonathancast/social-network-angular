a8e3165018db3b7382173478d4aed4c64900379b
Jonathan Cast <jonathanccast@fastmail.fm>
2017-06-08 12:57:34 -0500

    Try to fetch current user information and show login box on failure

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,45 @@
 
 var app = angular.module('albatross-social-network', ['ngMessages']);
 
-var myCtrl = function($scope) {
-    $scope.loginVisible = true;
-    $scope.mainVisible = false;
+var myCtrl = function($scope, socialNetwork) {
+    $scope.showLogin = function(k) {
+        this.login = { onlogin: k };
+        this.loginVisible = true;
+        this.mainVisible = false;
+    };
+
+    $scope.hideLogin = function() {
+        this.loginVisible = false;
+        this.mainVisible = true;
+    };
+
+    var setupUser = function() {
+        $scope.hideLogin();
+
+        socialNetwork.user().then(
+            function(user) { $scope.user = user },
+            function(err) {
+                if (err.status === 401) {
+                    $scope.showLogin(setupUser);
+                } else {
+                    console.log('Got weird error; help', err.status, err);
+                }
+            }
+        );
+    };
+
+    setupUser();
 };
 
-app.controller('myCtrl', [ '$scope', myCtrl ]);
+var socialNetwork = function($http) {
+    var res = {};
+
+    res.user = function() {
+        return $http.get('/api/user');
+    }
+
+    return res;
+};
+
+app.factory('socialNetwork', [ '$http', socialNetwork ]);
+app.controller('myCtrl', [ '$scope', 'socialNetwork', myCtrl ]);
